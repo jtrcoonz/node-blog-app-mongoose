4a76efa5233c580784d29819c8a9107e0198982f
Jacob Coons <jacobtcoons@gmail.com>
2018-09-26 17:28:33 -0700

    Initial commit with config and model files made and set up

---
+++ server.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const { DATABASE_URL, PORT } = require('./config');
+const { blogPost } = require('./models');
