a797d65ff20ebfb49bb707c2373b3f85c7d09679
Jacob Coons <jacobtcoons@gmail.com>
2018-10-04 14:17:07 -0700

    Added users and comments to blogging app, new endpoints and middlewear to server.js, new models to models.js

--- models.js
+++ models.js
@@ -3,14 +3,32 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
+const authorSchema = mongoose.Schema({
+	firstName: String,
+	lastName: String,
+	userName: {
+		type: String,
+		unique: true
+	}
+});
+
+commentSchema = mongoose.Schema({ content: String });
+
 const blogPostSchema = mongoose.Schema({
-	author: {
-		firstName: String,
-		lastName: String
-	},
-	title: {type: String, required: true},
-	content: {type: String},
-	created: {type: Date, default: Date.now}
+	title: String,
+	content: String,
+	author: { type: mongoose.Schema.Types.ObjectId, ref: 'Author' },
+	comments: [commentSchema]
+});
+
+blogPostSchema.pre('find', function(next) {
+	this.populate('author');
+	next();
+});
+
+blogPostSchema.pre('findOne', function(next) {
+	this.populate('author');
+	next();
 });
 
 blogPostSchema.virtual('authorName').get(function() {
@@ -23,13 +41,14 @@ blogPostSchema.methods.serialize = function() {
 		author: this.authorName,
 		content: this.content,
 		title: this.title,
-		created: this.created
+		comments: this.comments
 	};
 };
 
+const Author = mongoose.model('Author', authorSchema, 'Authors');
 const BlogPost = mongoose.model('BlogPost', blogPostSchema, 'BlogPosts');
 
-module.exports = { BlogPost };
+module.exports = { Author, BlogPost };
 
 
 
